ab619de5ae6aac3cdba65f1e62fa450f32360af1
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-07-06 18:29:10 +0530

    Update Usermodel.js

--- Usermodel.js
+++ Usermodel.js
@@ -1,7 +1,36 @@
 import mongoose from 'mongoose'
-import { type } from 'os'
-// import { type } from 'os'
-// const { type } = require('os')
+const TripSchema = mongoose.Schema({
+            role:{
+                type:String,
+                required:true
+            },
+
+            source:{
+                type:String,
+                required:true
+            },
+
+            destination:{
+                type:String,
+                required:true
+            },
+
+            duration:{
+                type:Number,
+                required:true
+            },
+
+            fare:{
+                type:Number,
+                required:true
+            },
+        },
+
+        {
+            _id:false
+        }
+
+)
 
 const AppUserSchema = mongoose.Schema({
     phoneNumber:{
@@ -19,10 +48,12 @@ const AppUserSchema = mongoose.Schema({
         required:true
     },
 
-    role:{
-        type:String,
-        required:true
+    trips:{
+        type:[TripSchema],    
     }
+
+
+
 })
 
 export const AppUser = mongoose.model("AppUser",AppUserSchema)
